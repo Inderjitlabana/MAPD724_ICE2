b2a1c240de965802f71f09562daf36292c41367b
Inderjitlabana <73485599+Inderjitlabana@users.noreply.github.com>
2022-01-30 21:17:09 -0500

    Added ICE2 code files

---
+++ ICE2/ICE2/GameScene.swift
@@ -0,0 +1,96 @@
+//
+//  GameScene.swift
+//  ICE1
+//
+//  Created by Inderjit Singh on 2022-01-23.
+//
+
+import UIKit
+import AVFoundation
+import SpriteKit
+import GameplayKit
+
+let screenSize = UIScreen.main.bounds
+var screenWidth: CGFloat?
+var screenHeight: CGFloat?
+
+class GameScene: SKScene {
+    
+    var ocean: Ocean?
+    var island: Island?
+    var plane: Plane?
+    var clouds: [Cloud] = []
+    
+    override func didMove(to view: SKView)
+    {
+        screenWidth = frame.width
+        screenHeight = frame.height
+        
+        name = "GAME"
+        
+        ocean = Ocean()
+        ocean?.position = CGPoint(x: 0, y: 700)
+        addChild(ocean!)
+        
+        island = Island()
+        addChild(island!)
+        
+        plane = Plane()
+        plane?.position = CGPoint(x: 0, y: -495)
+        addChild(plane!)
+        
+        for index in 0...2
+        {
+            let cloud: Cloud = Cloud()
+            clouds.append(cloud)
+            addChild(clouds[index])
+        }
+                
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint)
+    {
+        plane?.TouchMove(newPos: CGPoint(x: pos.x, y: -495))
+    }
+    
+    func touchMoved(toPoint pos : CGPoint)
+    {
+        plane?.TouchMove(newPos: CGPoint(x: pos.x, y: -495))
+    }
+    
+    func touchUp(atPoint pos : CGPoint)
+    {
+        plane?.TouchMove(newPos: CGPoint(x: pos.x, y: -495))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval)
+    {
+        ocean?.Update()
+        plane?.Update()
+        island?.Update()
+        for cloud in clouds
+        {
+            cloud.Update()
+        }
+    }
+}
